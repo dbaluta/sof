c789be1ecbfde17867affa30b39ef6b4bec024ba
Daniel Baluta <daniel.baluta@nxp.com>
2019-01-21 12:46:04 +0200

    ipc
    Signed-off-by: Daniel Baluta <daniel.baluta@nxp.com>

--- src/platform/imx8/platform.c
+++ src/platform/imx8/platform.c
@@ -37,6 +37,7 @@
 #include <platform/clk.h>
 #include <platform/timer.h>
 #include <platform/pmc.h>
+#include <platform/mu.h>
 #include <platform/platcfg.h>
 #include <uapi/ipc/info.h>
 #include <sof/mailbox.h>
@@ -57,14 +58,112 @@
 #include <string.h>
 #include <version.h>
 
+static const struct sof_ipc_fw_ready ready
+	__attribute__((section(".fw_ready"))) = {
+	.hdr = {
+		.cmd = SOF_IPC_FW_READY,
+		.size = sizeof(struct sof_ipc_fw_ready),
+	},
+	/* dspbox is for DSP initiated IPC, hostbox is for host initiated IPC */
+	.version = {
+		.hdr.size = sizeof(struct sof_ipc_fw_version),
+		.micro = SOF_MICRO,
+		.minor = SOF_MINOR,
+		.major = SOF_MAJOR,
+#ifdef DEBUG_BUILD
+		/* only added in debug for reproducability in releases */
+		.build = SOF_BUILD,
+		.date = __DATE__,
+		.time = __TIME__,
+#endif
+		.tag = SOF_TAG,
+		.abi_version = SOF_ABI_VERSION,
+	},
+	.debug = DEBUG_SET_FW_READY_FLAGS
+};
+
+#define NUM_IMX_WINDOWS		6
+
+static const struct sof_ipc_window sram_window = {
+	.ext_hdr	= {
+		.hdr.cmd = SOF_IPC_FW_READY,
+		.hdr.size = sizeof(struct sof_ipc_window) +
+			sizeof(struct sof_ipc_window_elem) * NUM_IMX_WINDOWS,
+		.type	= SOF_IPC_EXT_WINDOW,
+	},
+	.num_windows	= NUM_IMX_WINDOWS,
+	.window	= {
+		{
+			.type	= SOF_IPC_REGION_UPBOX,
+			.id	= 0,	/* map to host window 0 */
+			.flags	= 0, // TODO: set later
+			.size	= MAILBOX_DSPBOX_SIZE,
+			.offset	= MAILBOX_DSPBOX_OFFSET,
+		},
+		{
+			.type	= SOF_IPC_REGION_DOWNBOX,
+			.id	= 0,	/* map to host window 0 */
+			.flags	= 0, // TODO: set later
+			.size	= MAILBOX_HOSTBOX_SIZE,
+			.offset	= MAILBOX_HOSTBOX_OFFSET,
+		},
+		{
+			.type	= SOF_IPC_REGION_DEBUG,
+			.id	= 0,	/* map to host window 0 */
+			.flags	= 0, // TODO: set later
+			.size	= MAILBOX_DEBUG_SIZE,
+			.offset	= MAILBOX_DEBUG_OFFSET,
+		},
+		{
+			.type	= SOF_IPC_REGION_TRACE,
+			.id	= 0,	/* map to host window 0 */
+			.flags	= 0, // TODO: set later
+			.size	= MAILBOX_TRACE_SIZE,
+			.offset	= MAILBOX_TRACE_OFFSET,
+		},
+		{
+			.type	= SOF_IPC_REGION_STREAM,
+			.id	= 0,	/* map to host window 0 */
+			.flags	= 0, // TODO: set later
+			.size	= MAILBOX_STREAM_SIZE,
+			.offset	= MAILBOX_STREAM_OFFSET,
+		},
+		{
+			.type	= SOF_IPC_REGION_EXCEPTION,
+			.id	= 0,	/* map to host window 0 */
+			.flags	= 0, // TODO: set later
+			.size	= MAILBOX_EXCEPTION_SIZE,
+			.offset	= MAILBOX_EXCEPTION_OFFSET,
+		},
+	},
+};
+
 struct timer *platform_timer;
+
 
 int platform_boot_complete(uint32_t boot_message)
 {
+	mailbox_dspbox_write(0, &ready, sizeof(ready));
+	mailbox_dspbox_write(sizeof(ready), &sram_window,
+			     sram_window.ext_hdr.hdr.size);
+
+	/* now interrupt host to tell it we are done booting */
+//	shim_write(SHIM_IPCDL, SOF_IPC_FW_READY | outbox);
+//	shim_write(SHIM_IPCDH, SHIM_IPCDH_BUSY);
+	imx_mu_write(IMX_MU_xTRn(0), SOF_IPC_FW_READY);
+
+	/* boot now complete so we can relax the CPU */
+	/* For now skip this to gain more processing performance
+	 * for SRC component.
+	 */
+	/* clock_set_freq(CLK_CPU, CLK_DEFAULT_CPU_HZ); */
+
 	return 0;
 }
 
 int platform_init(struct sof *sof)
 {
+	ipc_init(sof);
+
 	return 0;
 }
