d31f61b281e5bbde0d7547e840278d2a1901715e
Daniel Baluta <daniel.baluta@nxp.com>
2019-02-04 13:03:01 +0200

    imx8: fix mailbox
    Signed-off-by: Daniel Baluta <daniel.baluta@nxp.com>

--- src/platform/imx8/include/platform/mailbox.h
+++ src/platform/imx8/include/platform/mailbox.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, Intel Corporation
+ * Copyright (c) 2017, Intel Corporation
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -26,6 +26,8 @@
  * POSSIBILITY OF SUCH DAMAGE.
  *
  * Author: Liam Girdwood <liam.r.girdwood@linux.intel.com>
+ *         Keyon Jie <yang.jie@linux.intel.com>
+ *         Rander Wang <rander.wang@intel.com>
  */
 
 #ifndef __INCLUDE_PLATFORM_MAILBOX__
@@ -33,40 +35,43 @@
 
 #include <platform/memory.h>
 
-#define MAILBOX_HOST_OFFSET	0x144000
+/*
+ * The Window Region on i.MX8 SRAM is organised like this :-
+ * +--------------------------------------------------------------------------+
+ * | Offset              | Region         |  Size                             |
+ * +---------------------+----------------+-----------------------------------+
+ * | SRAM_TRACE_BASE     | Trace Buffer   |  SRAM_TRACE_SIZE                  |
+ * +---------------------+----------------+-----------------------------------+
+ * | SRAM_DEBUG_BASE     | Debug data     |  SRAM_DEBUG_SIZE                  |
+ * +---------------------+----------------+-----------------------------------+
+ * | SRAM_INBOX_BASE     | Inbox          |  SRAM_INBOX_SIZE                  |
+ * +---------------------+----------------+-----------------------------------+
+ * | SRAM_OUTBOX_BASE    | Outbox         |  SRAM_MAILBOX_SIZE                |
+ * +---------------------+----------------+-----------------------------------+
+ */
 
-#define MAILBOX_DSPBOX_OFFSET	0x0
-#define MAILBOX_DSPBOX_SIZE	0x400
-#define MAILBOX_DSPBOX_BASE \
-	(MAILBOX_BASE + MAILBOX_DSPBOX_OFFSET)
+#define MAILBOX_DSPBOX_SIZE		SRAM_OUTBOX_SIZE
+#define MAILBOX_DSPBOX_BASE		SRAM_OUTBOX_BASE
+#define MAILBOX_DSPBOX_OFFSET		SRAM_OUTBOX_OFFSET
 
-#define MAILBOX_HOSTBOX_OFFSET	MAILBOX_DSPBOX_SIZE
-#define MAILBOX_HOSTBOX_SIZE	0x400
-#define MAILBOX_HOSTBOX_BASE \
-	(MAILBOX_BASE + MAILBOX_HOSTBOX_OFFSET)
+#define MAILBOX_HOSTBOX_SIZE		SRAM_INBOX_SIZE
+#define MAILBOX_HOSTBOX_BASE		SRAM_INBOX_BASE
+#define MAILBOX_HOSTBOX_OFFSET		SRAM_INBOX_OFFSET
 
-#define MAILBOX_EXCEPTION_OFFSET \
-	(MAILBOX_HOSTBOX_SIZE + MAILBOX_DSPBOX_SIZE)
-#define MAILBOX_EXCEPTION_SIZE	0x100
-#define MAILBOX_EXCEPTION_BASE \
-	(MAILBOX_BASE + MAILBOX_EXCEPTION_OFFSET)
+#define MAILBOX_DEBUG_SIZE		SRAM_DEBUG_SIZE
+#define MAILBOX_DEBUG_BASE		SRAM_DEBUG_BASE
+#define MAILBOX_DEBUG_OFFSET		SRAM_DEBUG_OFFSET
 
-#define MAILBOX_DEBUG_OFFSET \
-	(MAILBOX_EXCEPTION_SIZE + MAILBOX_EXCEPTION_OFFSET)
-#define MAILBOX_DEBUG_SIZE	0x100
-#define MAILBOX_DEBUG_BASE \
-	(MAILBOX_BASE + MAILBOX_DEBUG_OFFSET)
+#define MAILBOX_TRACE_SIZE		SRAM_TRACE_SIZE
+#define MAILBOX_TRACE_BASE		SRAM_TRACE_BASE
+#define MAILBOX_TRACE_OFFSET		SRAM_TRACE_OFFSET
 
-#define MAILBOX_STREAM_OFFSET \
-	(MAILBOX_DEBUG_SIZE + MAILBOX_DEBUG_OFFSET)
-#define MAILBOX_STREAM_SIZE	0x200
-#define MAILBOX_STREAM_BASE \
-	(MAILBOX_BASE + MAILBOX_STREAM_OFFSET)
+#define MAILBOX_EXCEPTION_SIZE		SRAM_EXCEPT_SIZE
+#define MAILBOX_EXCEPTION_BASE		SRAM_EXCEPT_BASE
+#define MAILBOX_EXCEPTION_OFFSET	SRAM_DEBUG_SIZE
 
-#define MAILBOX_TRACE_OFFSET \
-	(MAILBOX_STREAM_SIZE + MAILBOX_STREAM_OFFSET)
-#define MAILBOX_TRACE_SIZE	0x380
-#define MAILBOX_TRACE_BASE \
-	(MAILBOX_BASE + MAILBOX_TRACE_OFFSET)
+#define MAILBOX_STREAM_SIZE		SRAM_STREAM_SIZE
+#define MAILBOX_STREAM_BASE		SRAM_STREAM_BASE
+#define MAILBOX_STREAM_OFFSET		SRAM_STREAM_OFFSET
 
 #endif
