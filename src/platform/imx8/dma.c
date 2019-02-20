907df03043a640257615b8efe1582a2739eb35cf
Daniel Baluta <daniel.baluta@nxp.com>
2019-02-20 09:36:14 +0200

    imx8: dma: install EDMA
    Signed-off-by: Daniel Baluta <daniel.baluta@nxp.com>

--- src/platform/imx8/dma.c
+++ src/platform/imx8/dma.c
@@ -29,177 +29,35 @@
  */
 
 #include <sof/dma.h>
-#include <sof/dw-dma.h>
+#include <sof/edma.h>
+
 #include <platform/memory.h>
 #include <platform/interrupt.h>
 #include <platform/dma.h>
 #include <stdint.h>
 #include <string.h>
 
-#if 0
-static struct dw_drv_plat_data dmac0 = {
-	.chan[0] = {
-		.class	= 6,
-		.weight = 0,
-	},
-	.chan[1] = {
-		.class	= 6,
-		.weight = 0,
-	},
-	.chan[2] = {
-		.class	= 6,
-		.weight = 0,
-	},
-	.chan[3] = {
-		.class	= 6,
-		.weight = 0,
-	},
-	.chan[4] = {
-		.class	= 6,
-		.weight = 0,
-	},
-	.chan[5] = {
-		.class	= 6,
-		.weight = 0,
-	},
-	.chan[6] = {
-		.class	= 6,
-		.weight = 0,
-	},
-	.chan[7] = {
-		.class	= 6,
-		.weight = 0,
-	},
-};
-
-static struct dw_drv_plat_data dmac1 = {
-	.chan[0] = {
-		.class	= 7,
-		.weight = 0,
-	},
-	.chan[1] = {
-		.class	= 7,
-		.weight = 0,
-	},
-	.chan[2] = {
-		.class	= 7,
-		.weight = 0,
-	},
-	.chan[3] = {
-		.class	= 7,
-		.weight = 0,
-	},
-	.chan[4] = {
-		.class	= 7,
-		.weight = 0,
-	},
-	.chan[5] = {
-		.class	= 7,
-		.weight = 0,
-	},
-	.chan[6] = {
-		.class	= 7,
-		.weight = 0,
-	},
-	.chan[7] = {
-		.class	= 7,
-		.weight = 0,
-	},
-};
-
-#if defined CONFIG_CHERRYTRAIL
-static struct dw_drv_plat_data dmac2 = {
-	.chan[0] = {
-		.class	= 7,
-		.weight = 0,
-	},
-	.chan[1] = {
-		.class	= 7,
-		.weight = 0,
-	},
-	.chan[2] = {
-		.class	= 7,
-		.weight = 0,
-	},
-	.chan[3] = {
-		.class	= 7,
-		.weight = 0,
-	},
-	.chan[4] = {
-		.class	= 7,
-		.weight = 0,
-	},
-	.chan[5] = {
-		.class	= 7,
-		.weight = 0,
-	},
-	.chan[6] = {
-		.class	= 7,
-		.weight = 0,
-	},
-	.chan[7] = {
-		.class	= 7,
-		.weight = 0,
-	},
-};
-#endif
-
 struct dma dma[PLATFORM_NUM_DMACS] = {
 {
 	.plat_data = {
-		.id		= DMA_ID_DMAC0,
+		.id		= DMA_ID_EDMA0,
 		.dir		= DMA_DIR_MEM_TO_MEM | DMA_DIR_MEM_TO_DEV |
 				  DMA_DIR_DEV_TO_MEM | DMA_DIR_DEV_TO_DEV |
 				  DMA_DIR_HMEM_TO_LMEM | DMA_DIR_LMEM_TO_HMEM,
 		.caps		= DMA_CAP_GP_HP,
-		.devs		= DMA_DEV_SSP | DMA_DEV_DMIC | DMA_DEV_HOST,
-		.base		= DMA0_BASE,
-		.channels	= 8,
-		.irq		= IRQ_NUM_EXT_DMAC0,
-		.drv_plat_data	= &dmac0,
+		.devs		= DMA_DEV_SAI | DMA_DEV_HOST,
+		.base		= EDMA0_BASE,
+		.channels	= 32,
+		.irq		= IRQ_NUM_IRQSTR_DSP6,
+		//.drv_plat_data	= &dmac0,
 	},
-	.ops		= &dw_dma_ops,
+	.ops		= &edma_ops,
 },
-{
-	.plat_data = {
-		.id		= DMA_ID_DMAC1,
-		.dir		= DMA_DIR_MEM_TO_MEM | DMA_DIR_MEM_TO_DEV |
-				  DMA_DIR_DEV_TO_MEM | DMA_DIR_DEV_TO_DEV |
-				  DMA_DIR_HMEM_TO_LMEM | DMA_DIR_LMEM_TO_HMEM,
-		.caps		= DMA_CAP_GP_HP,
-		.devs		= DMA_DEV_SSP | DMA_DEV_DMIC | DMA_DEV_HOST,
-		.base		= DMA1_BASE,
-		.channels	= 8,
-		.irq		= IRQ_NUM_EXT_DMAC1,
-		.drv_plat_data	= &dmac1,
-	},
-	.ops		= &dw_dma_ops,
-},
-#if defined CONFIG_CHERRYTRAIL
-{
-	.plat_data = {
-		.id		= DMA_ID_DMAC2,
-		.dir		= DMA_DIR_MEM_TO_MEM | DMA_DIR_MEM_TO_DEV |
-				  DMA_DIR_DEV_TO_MEM | DMA_DIR_DEV_TO_DEV |
-				  DMA_DIR_HMEM_TO_LMEM | DMA_DIR_LMEM_TO_HMEM,
-		.caps		= DMA_CAP_GP_HP,
-		.devs		= DMA_DEV_SSP | DMA_DEV_DMIC | DMA_DEV_HOST,
-		.base		= DMA2_BASE,
-		.channels	= 8,
-		.irq		= IRQ_NUM_EXT_DMAC2,
-		.drv_plat_data	= &dmac2,
-	},
-	.ops		= &dw_dma_ops,
-},
-#endif
 };
 
-#endif
-
 /* Initialize all platform DMAC's */
-int dmac_init(void)
+int edma_init(void)
 {
-#if 0
 	int i;
 	/* no probing before first use */
 
@@ -211,6 +69,6 @@ int dmac_init(void)
 
 	/* tell the lib DMAs are ready to use */
 	dma_install(dma, ARRAY_SIZE(dma));
-#endif
+
 	return 0;
 }
