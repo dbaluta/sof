4d75fefa8f36502b744974f6da98aae12c5bc0e1
Daniel Baluta <daniel.baluta@nxp.com>
2020-07-27 17:56:00 +0300

    to compoile xtensa-cc
    Signed-off-by: Daniel Baluta <daniel.baluta@nxp.com>

--- scripts/xtensa-build-all.sh
+++ scripts/xtensa-build-all.sh
@@ -240,8 +240,12 @@ do
 
 	if [ $XTENSA_TOOLS_ROOT ]
 	then
-		XTENSA_TOOLS_DIR="$XTENSA_TOOLS_ROOT/install/tools/$XTENSA_TOOLS_VERSION"
-		XTENSA_BUILDS_DIR="$XTENSA_TOOLS_ROOT/install/builds/$XTENSA_TOOLS_VERSION"
+		#XTENSA_TOOLS_DIR="$XTENSA_TOOLS_ROOT/install/tools/$XTENSA_TOOLS_VERSION"
+		#XTENSA_BUILDS_DIR="$XTENSA_TOOLS_ROOT/install/builds/$XTENSA_TOOLS_VERSION"
+
+
+		XTENSA_TOOLS_DIR="$XTENSA_TOOLS_ROOT/Xtensa_Tool/tools/$XTENSA_TOOLS_VERSION"
+                XTENSA_BUILDS_DIR="$XTENSA_TOOLS_ROOT/Xtensa_Tool/builds/$XTENSA_TOOLS_VERSION"
 
 		# make sure the required version of xtensa tools is installed
 		if [ -d $XTENSA_TOOLS_DIR ]
